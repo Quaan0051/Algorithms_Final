97421844fc154308016cb1cb8b4c1eb7cb9065d4
Drak0031 <drak0031@algonquinlive.com>
2025-04-14 15:45:30 -0400

    started donkey, bull and alpaca

--- Assets/Scripts/Game/Alpaca.cs
+++ Assets/Scripts/Game/Alpaca.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
 
 public class Alpaca : Animal
 {
     [SerializeField] private Blackboard blackboard;
     [SerializeField] private AnimalSettings settings;
+
+    Target target;
 
     private void Awake()
     {
@@ -15,11 +18,21 @@ public class Alpaca : Animal
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    void LateUpdate()
+    {
+        Vector3 current = target.transform.position;
+        Vector3 previous = target.PreviousPosition;
+        Vector3 direction = (current - previous).normalized;
+        transform.forward = direction;
     }
 
     protected override void OnStart()
     {
+        target = GetComponent<Target>();
+
         Initialize(settings);
     }
 }
