2979c9ef3f8eb1bf33510aff67a3e53a59292c83
Drak0031 <drak0031@algonquinlive.com>
2025-04-15 19:32:42 -0400

    set up walking and eating for donkey and bull, and started rage for bull

--- Assets/Scripts/Game/Bull.cs
+++ Assets/Scripts/Game/Bull.cs
@@ -1,11 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
 
 public class Bull : Animal
 {
     [SerializeField] private Blackboard blackboard;
     [SerializeField] private AnimalSettings settings;
+
+    private float HungryTimer;
+    //private float RageTimer;
 
     private void Awake()
     {
@@ -15,11 +19,32 @@ public class Bull : Animal
     // Update is called once per frame
     void Update()
     {
-
+        
+        //if (RageTimer >= 0.0f && blackboard.GetValue<bool>("Rage") == false)
+            //RageTimer -= Time.deltaTime;
+            //if (RageTimer <= 0.0f)
+            //{
+            //    blackboard.SetValue<bool>("Rage", true);
+            //    RageTimer = Random.Range(20.0f, 40.0f);
+            //}
+        if (HungryTimer >= 0.0f && blackboard.GetValue<bool>("isHungry") == false)
+        {
+            HungryTimer -= Time.deltaTime;
+            if (HungryTimer <= 0.0f)
+            {
+                blackboard.SetValue<bool>("isHungry", true);
+                HungryTimer = Random.Range(45.0f, 90.0f);
+            }
+        }
+        //else 
+            //get rand chicken location
+            //modify blackboard, put chicken in target
     }
 
     protected override void OnStart()
     {
         Initialize(settings);
+        HungryTimer = Random.Range(45.0f, 90.0f);
+        //RageTimer = Random.Range(20.0f, 40.0f);
     }
 }
--- Assets/Scripts/Game/Donkey.cs
+++ Assets/Scripts/Game/Donkey.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
 
 public class Donkey : Animal
 {
     [SerializeField] private Blackboard blackboard;
     [SerializeField] private AnimalSettings settings;
+
+    private float HungryTimer;
 
     private void Awake()
     {
@@ -15,11 +18,20 @@ public class Donkey : Animal
     // Update is called once per frame
     void Update()
     {
-        
+        if (HungryTimer >= 0.0f && blackboard.GetValue<bool>("isHungry") == false)
+        {
+            HungryTimer -= Time.deltaTime;
+            if (HungryTimer <= 0.0f)
+            {
+                blackboard.SetValue<bool>("isHungry", true);
+                HungryTimer = Random.Range(15.0f, 30.0f);
+            }
+        }
     }
 
     protected override void OnStart()
     {
         Initialize(settings);
+        HungryTimer = Random.Range(15.0f, 30.0f);
     }
 }
